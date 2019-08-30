1a15c4276c3940705a24a61e6217dac09ba31e27
Ievgen Gavrysh <zhenya.ga@gmail.com>
2019-08-30 16:46:26 -0700

    Transfer uom service to reactive uom

--- src/test/java/guru/springframework/controllers/IngredientControllerTest.java
+++ src/test/java/guru/springframework/controllers/IngredientControllerTest.java
@@ -15,6 +15,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import reactor.core.publisher.Flux;
 
 import java.util.HashSet;
 
@@ -87,7 +88,7 @@ public class IngredientControllerTest {
 
         // when
         when(recipeService.findCommandById(anyString())).thenReturn(recipeCommand);
-        when(unitOfMeasureService.listAllUoms()).thenReturn(new HashSet<>());
+        when(unitOfMeasureService.listAllUoms()).thenReturn(Flux.just());
 
         // then
         mockMvc.perform(get("/recipe/1/ingredient/new"))
@@ -106,7 +107,7 @@ public class IngredientControllerTest {
 
         // when
         when(ingredientService.findByRecipeIdAndIngredientId(anyString(), anyString())).thenReturn(ingredientCommand);
-        when(unitOfMeasureService.listAllUoms()).thenReturn(new HashSet<>());
+        when(unitOfMeasureService.listAllUoms()).thenReturn(Flux.just());
 
         // then
         mockMvc.perform(get("/recipe/1/ingredient/2/update"))
