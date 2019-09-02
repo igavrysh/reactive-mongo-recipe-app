b502955b1d40f449c5c8acbae84fbe141a8bba22
Ievgen Gavrysh <zhenya.ga@gmail.com>
2019-09-02 07:56:57 -0700

    Added custom error page for valdiation errors

--- src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
+++ src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
@@ -2,28 +2,26 @@ package guru.springframework.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    /*
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormatException(Exception exception) {
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleNumberFormatException(Exception exception, Model model) {
 
         log.error("Number format exception");
         log.error(exception.getMessage());
 
-        ModelAndView modelAndView = new ModelAndView();
+        model.addAttribute("exception", exception);
 
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", exception);
-
-        return modelAndView;
+        return "400error";
     }
-    */
+
 }
